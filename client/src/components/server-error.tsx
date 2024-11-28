e8857b402a344046e98cf4c21c57390864b8997e
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-28 02:56:01 +0000

    responsive-ise the server error page

--- client/src/components/server-error.tsx
+++ client/src/components/server-error.tsx
@@ -1,58 +1,56 @@
-import type React from "react"
+import type * as React from "react"
 
-const styles: Record<string, React.CSSProperties> = {
-  error: {
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  desc: {
-    display: "flex",
-    height: "96px",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  h1: {
-    display: "inline-block",
-    margin: "0 20px 0 0",
-    paddingRight: 23,
-    fontSize: 48,
-    fontWeight: 500,
-    verticalAlign: "top",
-    borderRight: "1px solid hsl(var(--foreground) / 0.5)",
-  },
-  h2: {
-    fontSize: 28,
-    fontWeight: 400,
-    lineHeight: "28px",
-  },
-  wrap: {
-    display: "inline-block",
-  },
+import { cn } from "@/lib/utils"
+
+type ServerErrorCodeSectionProps = {
+  statusCode?: string
+} & React.HTMLAttributes<HTMLDivElement>
+
+function ServerErrorCodeSection({ statusCode, className, ...props }: ServerErrorCodeSectionProps) {
+  if (statusCode == null) return null
+  const sectionClassName = cn(
+    className,
+    "border-foreground/50",
+    "pb-4 mb-5 border-b text-7xl",
+    "lg:pb-0 lg:mb-0 lg:pr-5 lg:mr-5 lg:border-b-0 lg:border-r lg:text-5xl",
+  )
+  return (
+    <section className={sectionClassName} {...props}>
+      <h1 className="font-[family-name:var(--font-jetbrains-mono)] font-medium">{statusCode}</h1>
+    </section>
+  )
 }
 
-export type HttpErrorProps = {
+type ServerErrorMessageSectionProps = {
+  message: string
+} & React.HTMLAttributes<HTMLDivElement>
+
+function ServerErrorMessageSection({ message, className, ...props }: ServerErrorMessageSectionProps) {
+  return (
+    <section className={cn(className, "content-center inline-block text-2xl")} {...props}>
+      <h2 className="font-[family-name:var(--font-onest)] font-normal">{message}.</h2>
+    </section>
+  )
+}
+
+export type ServerErrorProps = {
   statusCode?: string
   message: string
 } & React.HTMLAttributes<HTMLDivElement>
 
-export function ServerErrorPage({ style, ...props }: HttpErrorProps) {
-  style ??= {}
-  style.minHeight = "100vh"
-  return <ServerError style={style} {...props} />
+export function ServerError({ statusCode, message, className, ...props }: ServerErrorProps) {
+  return (
+    <div className={cn(className, "flex flex-col justify-center items-center")} {...props}>
+      <div className="w-full min-h-[110px] lg:hidden" />
+      <main className="flex flex-col lg:flex-row text-center lg:text-left justify-center">
+        <ServerErrorCodeSection statusCode={statusCode} />
+        <ServerErrorMessageSection message={message} />
+      </main>
+      <div className="w-full min-h-[110px] lg:hidden" />
+    </div>
+  )
 }
 
-export function ServerError({ statusCode, message, style, ...props }: HttpErrorProps) {
-  return (
-    <main style={{ ...styles.error, ...style }} {...props}>
-      <div style={styles.desc}>
-        {statusCode ? <h1 style={styles.h1}>{statusCode}</h1> : null}
-        <div style={styles.wrap}>
-          <h2 style={styles.h2}>{message}.</h2>
-        </div>
-      </div>
-    </main>
-  )
+export function ServerErrorPage({ className, ...props }: ServerErrorProps) {
+  return <ServerError className={cn(className, "min-h-[100vh] m-auto mx-5")} {...props} />
 }
