6b0998876ab750f3b5c7cc4e4a7e05f16b30d07f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-28 03:59:04 +0000

    configure some metadata (missing favicon + opengraph image)

---
+++ client/src/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "lordfirespeed.dev",
+  url: "https://www.lordfirespeed.dev",
+  themeColor: "#4f35c4",
+  description: [
+    "Computer Science and Mathematics undergraduate employed part-time by Durham University.",
+    "Currently building anything and everything for Durham University Computing Society.",
+  ].join(" "),
+  openGraphImage: "/assets/opengraph-image.png",
+}
