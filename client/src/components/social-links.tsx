1d29d57d4369c24cbd3eab5490182b616a49c733
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-28 03:24:09 +0000

    add social links component

---
+++ client/src/components/social-links.tsx
@@ -0,0 +1,22 @@
+import type * as React from "react"
+
+import { type SocialNetworkProfile, socials } from "@/config/socials"
+import { cn } from "@/lib/utils"
+
+function SocialLink({ profile: { href, icon: ProfileIcon } }: { profile: SocialNetworkProfile }) {
+  return (
+    <a href={href}>
+      <ProfileIcon className="size-12" />
+    </a>
+  )
+}
+
+export function SocialLinks({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <article className={cn("flex flex-row", className)} {...props}>
+      {socials.map((profile) => (
+        <SocialLink key={profile.slug} profile={profile} />
+      ))}
+    </article>
+  )
+}
